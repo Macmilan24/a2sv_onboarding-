8092f6ab674dbdc0992b4fb6e8dd4dfd7a1a2915
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-02-06 22:43:57 +0300

    Created 'Dragons' by A2SV Hub

---
+++ 06-Feb-2025/Dragons 248234.py
@@ -0,0 +1,19 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+strength, t = map(int, input().split())
+dragons = []
+
+for _ in range(t):
+    dragon_strength, bonus = map(int, input().split())
+    dragons.append((dragon_strength, bonus))
+
+dragons.sort()
+
+for dragon_strength, bonus in dragons:
+    if strength > dragon_strength:
+        strength += bonus 
+    else:
+        print("NO")
+        exit() 
+
+print("YES")
