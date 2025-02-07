e96b21409719aa765a8d57e73a2cb105b2b56e53
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-02-07 19:21:40 +0300

    Created 'Same Differences' by A2SV Hub

---
+++ 07-Feb-2025/Same Differences 250363.py
@@ -0,0 +1,19 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+t = int(input())
+
+for _ in range(t):
+    length = int(input())
+    nums = list(map(int, input().split()))
+        
+    hashmap = {}
+    count = 0
+        
+    for i in range(length):
+        val = nums[i] - i
+        if val in hashmap:
+            count += hashmap[val]
+            hashmap[val] += 1
+        else:
+            hashmap[val] = 1
+    print(count)
