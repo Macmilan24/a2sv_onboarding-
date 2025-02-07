dc490492491e7e815c8392d673b259d1e1c89527
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-02-07 17:48:16 +0300

    Created 'Majority Element' by A2SV Hub

---
+++ 07-Feb-2025/Majority Element 250119.py
@@ -0,0 +1,9 @@
+# Problem: Majority Element - https://leetcode.com/problems/majority-element/description/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        nums.sort()
+        
+        return nums[len(nums) // 2]
+        
