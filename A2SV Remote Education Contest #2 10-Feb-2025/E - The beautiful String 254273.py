cfc074d0b8d041b1e7d3be4b8b787b574f346436
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-02-10 21:19:39 +0300

    Created 'E - The beautiful String' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/E - The beautiful String 254273.py
@@ -0,0 +1,39 @@
+# Problem: E - The beautiful String - https://codeforces.com/gym/586622/problem/E
+
+t = int(input())
+
+for _ in range(t):
+    
+    string = list(input().strip())
+    input_count = int(input())
+    length = len(string)
+    
+    count = 0
+    
+    for j in range(length - 3):
+        if string[j] == "1" and string[j + 1] == "1" and string[j + 2] == "0" and string[j + 3] == "0":
+            count += 1
+            
+    
+    for j in range(input_count):
+        
+        i, q = input().split()
+        
+        i = int(i) - 1
+        
+        if string[i] == q:
+            print("YES" if count > 0 else "NO")
+            continue
+        
+        for j in range(max(0, i - 3), min(i + 1, length - 3)):
+            if string[j] == "1" and string[j + 1] == "1" and string[j + 2] == "0" and string[j + 3] == "0":
+                count -= 1
+        
+        string[i] = q
+        
+        for j in range(max(0, i - 3), min(i + 1, length - 3)):
+            if string[j] == "1" and string[j + 1] == "1" and string[j + 2] == "0" and string[j + 3] == "0":
+                count += 1
+        
+        print("YES" if count > 0 else "NO")        
+                
