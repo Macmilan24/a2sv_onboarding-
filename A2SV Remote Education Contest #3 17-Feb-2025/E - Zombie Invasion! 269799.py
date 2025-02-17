6ec8a289a58fbb3052f473f3b2a026eaa2b2b724
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-02-17 22:45:42 +0300

    Created 'E - Zombie Invasion!' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/E - Zombie Invasion! 269799.py
@@ -0,0 +1,31 @@
+# Problem: E - Zombie Invasion! - https://codeforces.com/gym/588094/problem/E
+
+from collections import defaultdict
+
+for _ in range(int(input())):
+    n, k = list(map(int, input().split()))
+    damage = list(map(int, input().split()))
+    position = list(map(int, input().split()))
+
+    newDamage = defaultdict(int)
+    for i in range(n):
+        newDamage[abs(position[i])] += damage[i]
+
+    position = sorted(newDamage.keys())
+
+    prevPosition = 0
+    carry = 0
+
+    for i in range(len(position)):
+        distance = position[i] - prevPosition
+        total = (k * distance) + carry
+
+        if total < newDamage[position[i]]:
+            print("NO")
+            break 
+        
+        carry = total - newDamage[position[i]]  
+        prevPosition = position[i] 
+
+    else:
+        print("YES")
