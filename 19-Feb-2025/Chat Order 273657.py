48b66dc6dc408cda010e8bb57631982e032c8674
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-02-19 15:43:31 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 19-Feb-2025/Chat Order 273657.py
@@ -0,0 +1,13 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+t = int(input())
+chat_list = {}
+
+for _ in range(t):
+    chat = input().strip()
+    
+    if chat in chat_list:
+        del chat_list[chat]
+    chat_list[chat] = None
+
+print("\n".join(reversed(chat_list.keys())))
