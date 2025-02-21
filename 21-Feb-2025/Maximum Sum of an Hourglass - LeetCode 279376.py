98e888deefc2f1218d3c36200f1a6f776178bbd2
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-02-21 21:25:48 +0300

    Created 'Maximum Sum of an Hourglass - LeetCode' by A2SV Hub

---
+++ 21-Feb-2025/Maximum Sum of an Hourglass - LeetCode 279376.py
@@ -0,0 +1,19 @@
+# Problem: Maximum Sum of an Hourglass - LeetCode - https://leetcode.com/problems/maximum-sum-of-an-hourglass/description/
+
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        max_sum = 0
+        rows, cols = len(grid), len(grid[0])
+
+        for row in range(1, rows - 1):
+            for col in range(1, cols - 1):
+
+                hourglass = (
+                    grid[row - 1][col - 1] + grid[row - 1][col] + grid[row - 1][col + 1] +
+                    grid[row][col] +
+                    grid[row + 1][col - 1] + grid[row + 1][col] + grid[row + 1][col + 1]
+                )
+
+                max_sum = max(max_sum, hourglass)
+        
+        return max_sum
