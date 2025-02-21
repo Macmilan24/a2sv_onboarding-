74b9fdf07561a49b3d26462288ef5fdfec9c553e
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-02-21 20:40:34 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 21-Feb-2025/A. Beautiful Matrix 279194.py
@@ -0,0 +1,8 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+
+for i in range(5):
+    row = input().split()
+    if '1' in row:
+        print(abs(2 - row.index('1')) + abs(2 - i))
+        break
