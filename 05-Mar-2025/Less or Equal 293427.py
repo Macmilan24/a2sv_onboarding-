0d5fec2da263998c089b177a3f4e04db32965474
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-03-05 20:38:07 +0300

    Created 'Less or Equal' by A2SV Hub

---
+++ 05-Mar-2025/Less or Equal 293427.py
@@ -0,0 +1,25 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+n, k = map(int, input().split())
+
+nums = list(map(int, input().split()))
+
+nums.sort()
+
+
+
+if  k == 0:
+    if nums[0] > 1:
+        print(1)
+    else:
+        print(-1)
+else:
+    k_th = nums[k - 1]
+    count = sum(1 for num in nums if num <= k_th)
+    
+    if count == k:
+        print(k_th)
+    else:
+        print(- 1)
+
+    
