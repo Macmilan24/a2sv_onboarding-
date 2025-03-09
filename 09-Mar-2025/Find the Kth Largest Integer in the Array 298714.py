c547f92d1596e1e687e85831fd65aad43597b61b
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-03-09 19:07:01 +0300

    Created 'Find the Kth Largest Integer in the Array' by A2SV Hub

---
+++ 09-Mar-2025/Find the Kth Largest Integer in the Array 298714.py
@@ -0,0 +1,7 @@
+# Problem: Find the Kth Largest Integer in the Array - https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/
+
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        
+        numbers = sorted([int(num) for num in nums], reverse=True)
+        return str(numbers[k-1])
