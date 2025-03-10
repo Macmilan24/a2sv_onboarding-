52a1e40cf35a8c5d1aa4c3da999253aaa45ea71b
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-03-10 22:14:41 +0300

    Created 'F - Luffy’s Lineup Challenge' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/F - Luffy’s Lineup Challenge 300745.py
@@ -0,0 +1,26 @@
+# Problem: F - Luffy’s Lineup Challenge - https://codeforces.com/gym/594356/problem/F
+
+def solve():
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    swaps = []
+
+    for i in range(n):
+        if b[i] != a[i]:
+            j = i + 1
+            while j < n and b[j] != a[i]:
+                j += 1
+            
+            
+            while j > i:
+                b[j], b[j - 1] = b[j - 1], b[j]
+                swaps.append((j, j + 1))
+                j -= 1
+
+    print(len(swaps))
+    for p1, p2 in swaps:
+        print(p1, p2)
+
+solve()
