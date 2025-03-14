a320b8f909956e5a7d6bd92afa326d5a8c5498a1
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-03-14 13:05:41 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Mar-2025/Books 306378.py
@@ -0,0 +1,22 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+books =list(map(int, input().split()))
+book_count = 0
+time_used = 0
+left = 0
+
+
+for right in range(n):
+    
+    time_used += books[right]
+    
+    while time_used > t:
+        time_used -= books[left]
+        left += 1
+    
+    window = right - left + 1
+    book_count = max(book_count, window)
+    
+
+print(book_count)
