c7f3ad50211027e7b69f584c0d4b06f39b0d25d6
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-03-15 23:04:41 +0300

    Created 'Divisibility by 2^n' by A2SV Hub

---
+++ 15-Mar-2025/Divisibility by 2^n 308008.py
@@ -0,0 +1,34 @@
+# Problem: Divisibility by 2^n - https://codeforces.com/contest/1744/problem/D
+
+def count_twos(x):
+    twos = 0
+    while x % 2 == 0:
+        twos += 1
+        x //= 2
+    return twos
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+
+    current_twos = sum(count_twos(x) for x in a)
+    
+    extra = [count_twos(i + 1) for i in range(n)]  
+    extra.sort(reverse=True)  
+
+    if current_twos >= n:
+        print(0)
+    elif current_twos + sum(extra) < n:
+        print(-1)
+    else:
+        need = n - current_twos
+        ops = 0
+        total_extra = 0
+        for e in extra:
+            total_extra += e
+            ops += 1
+            if total_extra >= need:
+                print(ops)
+                break
