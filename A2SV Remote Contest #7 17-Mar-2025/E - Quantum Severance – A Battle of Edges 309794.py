ef040caf72e660f1c4878e306b4ad936595b05c0
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-03-17 23:18:28 +0300

    Created 'E - Quantum Severance – A Battle of Edges' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/E - Quantum Severance – A Battle of Edges 309794.py
@@ -0,0 +1,30 @@
+# Problem: E - Quantum Severance – A Battle of Edges - https://codeforces.com/gym/596004/problem/E
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    p_sum = [a[0] if a[0] > 0 else 0]
+    n_sum = [abs(a[-1] if a[-1] < 0 else 0)]
+
+    for i in range(1, n):
+        p_val = 0
+        n_val = 0
+        
+        if a[i] > 0:
+            p_val = a[i]
+        
+        if a[-(i + 1)] < 0:
+            n_val = abs(a[-(i + 1)])
+        
+        p_sum.append(p_sum[-1] + p_val)
+        n_sum.append(n_sum[-1] + n_val)
+    
+    n_sum.reverse()
+    max_val = 0
+    for i in range(n):
+        cur_max = n_sum[i] + p_sum[i]
+        max_val = max(max_val, cur_max)
+    
+    print(max_val)
