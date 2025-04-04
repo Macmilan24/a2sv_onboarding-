2359c54b3cdc4840237ee6a31f583f98c82cb654
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-04-04 19:40:49 +0300

    Created 'Kefa and Company' by A2SV Hub

---
+++ 04-Apr-2025/Kefa and Company 326090.py
@@ -0,0 +1,19 @@
+# Problem: Kefa and Company - https://codeforces.com/problemset/problem/580/B
+
+n, d = map(int, input().split())
+friends = [tuple(map(int, input().split())) for _ in range(n)]
+
+friends.sort()
+
+max_friendship = 0
+current_friendship = 0
+left = 0
+
+for right in range(n):
+    while friends[right][0] - friends[left][0] >= d:
+        current_friendship -= friends[left][1]
+        left += 1
+    current_friendship += friends[right][1]
+    max_friendship = max(max_friendship, current_friendship)
+
+print(max_friendship)
