65470a99c63340c9446935fcb72b8f03e14c8ec3
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-04-06 22:25:41 +0300

    Created 'C - Clone the Troops' by A2SV Hub

---
+++ A2SV Remote Contest #8 06-Apr-2025/C - Clone the Troops 327490.py
@@ -0,0 +1,25 @@
+# Problem: C - Clone the Troops - https://codeforces.com/gym/599884/problem/C
+
+t = int(input())
+p = 10**9 + 7
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    s = sum(a)
+    curr_max = 0
+    m = 0
+    for x in a:
+        curr_max = max(0, curr_max + x)
+        m = max(m, curr_max)
+    if m > 0:
+        t = 1
+        for _ in range(k):
+            t = (t * 2) % p
+        t = (t - 1 + p) % p
+        s = (s + t * m) % p
+    else:
+        s = s % p
+    if s < 0:
+        s += p
+    print(s)
