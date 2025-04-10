0facad412fc2d2fe827532b169342cdb4bb42628
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-04-10 23:34:20 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 10-Apr-2025/Ehab Is an Odd Person 331398.py
@@ -0,0 +1,18 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n = int(input())
+a = list(map(int, input().split()))
+
+odd = False
+even = False
+
+for i in range(len(a)):
+    if a[i] % 2 == 0:
+        even = True
+    else:
+        odd = True
+
+if odd and even:
+    a.sort()
+
+print(*a)
