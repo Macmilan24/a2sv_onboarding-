07c1b66994b7e72fadbb439f82840f8359da473f
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-04-14 20:38:27 +0300

    Created 'E - Keep the order' by A2SV Hub

---
+++ A2SV Remote Contest #8 14-Apr-2025/E - Keep the order 333830.py
@@ -0,0 +1,18 @@
+# Problem: E - Keep the order - https://codeforces.com/gym/599884/problem/E
+
+n = int(input())
+enter = list(map(int, input().split()))
+exit = list(map(int, input().split()))
+
+exit_pos = {troop: i for i, troop in enumerate(exit)}
+
+fired = 0
+max_exit = -1
+
+for troop in enter:
+    if exit_pos[troop] > max_exit:
+        max_exit = exit_pos[troop]
+    else:
+        fired += 1
+
+print(fired)
