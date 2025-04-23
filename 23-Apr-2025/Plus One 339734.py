50d3c3d761ee30531ce4f463221c381e38f707f7
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-04-23 21:06:02 +0300

    Created 'Plus One' by A2SV Hub

---
+++ 23-Apr-2025/Plus One 339734.py
@@ -0,0 +1,19 @@
+# Problem: Plus One - https://leetcode.com/problems/plus-one/
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+
+            if i == 0:
+                digits.insert(0, 1)
+                return  digits
+
+
+
+        
