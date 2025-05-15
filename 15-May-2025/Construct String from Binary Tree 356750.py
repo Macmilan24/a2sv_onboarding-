0badc5bd2526b2913eb92b4206b0b0a7bf196427
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-05-15 23:13:23 +0300

    Created 'Construct String from Binary Tree' by A2SV Hub

---
+++ 15-May-2025/Construct String from Binary Tree 356750.py
@@ -0,0 +1,19 @@
+# Problem: Construct String from Binary Tree - https://leetcode.com/problems/construct-string-from-binary-tree/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if not root:
+            return ""
+        
+        if not root.left and not root.right:
+            return str(root.val)
+        if not root.right:
+            return f"{root.val}({self.tree2str(root.left)})"
+            
+        return f"{root.val}({self.tree2str(root.left)})({self.tree2str(root.right)})"
