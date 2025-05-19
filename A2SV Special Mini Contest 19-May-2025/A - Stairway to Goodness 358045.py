10a9e4e40de6596752533d87a23d8b43cb2b4835
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-05-19 18:54:01 +0300

    Created 'A - Stairway to Goodness' by A2SV Hub

---
+++ A2SV Special Mini Contest 19-May-2025/A - Stairway to Goodness 358045.py
@@ -0,0 +1,19 @@
+# Problem: A - Stairway to Goodness - https://codeforces.com/gym/608569/problem/A
+
+def max_good_array_length(l, r):
+    left, right = 1, 2 * (10**9)
+    max_len = 1
+    while left <= right:
+        mid = (left + right) // 2
+        total = (mid - 1) * mid // 2
+        if l + total <= r:
+            max_len = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return max_len
+
+t = int(input())
+for _ in range(t):
+    l, r = map(int, input().split())
+    print(max_good_array_length(l, r))
