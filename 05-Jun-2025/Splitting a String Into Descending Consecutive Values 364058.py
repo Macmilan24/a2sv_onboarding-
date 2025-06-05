9d4c95d1b6ccb4bcab854fc906ae532b8a3d7782
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-06-05 21:44:05 +0300

    Created 'Splitting a String Into Descending Consecutive Values' by A2SV Hub

---
+++ 05-Jun-2025/Splitting a String Into Descending Consecutive Values 364058.py
@@ -0,0 +1,22 @@
+# Problem: Splitting a String Into Descending Consecutive Values - https://leetcode.com/problems/splitting-a-string-into-descending-consecutive-values/
+
+class Solution:
+    def splitString(self, s: str) -> bool:
+        
+        def backtrack(idx, prev):
+            if idx == len(s):
+                return True
+            
+            for i in range(idx, len(s)):
+                val = int(s[idx : i + 1])
+                if val + 1 == prev and backtrack(i + 1, val):
+                    return True
+            
+            return False
+        
+        for i in range(len(s) - 1):
+            val = int(s[:i + 1])
+            if backtrack(i + 1, val):
+                return True
+        
+        return False
