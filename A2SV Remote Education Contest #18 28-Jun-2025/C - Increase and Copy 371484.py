255b80541bcb75003dc19c1ac9a35bdf68827da5
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-06-28 22:59:30 +0300

    Created 'C - Increase and Copy' by A2SV Hub

---
+++ A2SV Remote Education Contest #18 28-Jun-2025/C - Increase and Copy 371484.py
@@ -0,0 +1,27 @@
+# Problem: C - Increase and Copy - https://codeforces.com/gym/616066/problem/C
+
+import math
+
+
+def solve():
+    n = int(input())
+    if n == 1:
+        print(0)
+        return
+
+    ans = float("inf")
+
+    limit = int(math.sqrt(n)) + 2
+    for x in range(1, limit):
+        increase_moves = x - 1
+
+        copy_moves = (n + x - 1) // x - 1
+
+        ans = min(ans, increase_moves + copy_moves)
+
+    print(ans)
+
+
+t = int(input())
+for _ in range(t):
+    solve()
