853df14fa92fae3f28b695354581ae496b2b83b2
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-07-07 23:13:03 +0300

    Created 'A - Twice' by A2SV Hub

---
+++ A2SV Contest #21 07-Jul-2025/A - Twice 374397.py
@@ -0,0 +1,23 @@
+# Problem: A - Twice - https://codeforces.com/gym/619446/problem/A
+
+from collections import Counter
+
+def solve():
+
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    counts = Counter(a)
+
+    score = 0
+
+    for count in counts.values():
+        score += count // 2
+
+    print(score)
+
+
+t = int(input())
+
+for _ in range(t):
+    solve()
