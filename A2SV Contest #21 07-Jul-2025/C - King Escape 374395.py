655840f98054d5c9805a9ea6996bf31df76eb5a9
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-07-07 23:12:54 +0300

    Created 'C - King Escape' by A2SV Hub

---
+++ A2SV Contest #21 07-Jul-2025/C - King Escape 374395.py
@@ -0,0 +1,21 @@
+# Problem: C - King Escape - https://codeforces.com/gym/619446/problem/C
+
+def solve():
+    n = int(input())
+    ax, ay = map(int, input().split())
+    bx, by = map(int, input().split())
+    cx, cy = map(int, input().split())
+
+    is_king_x_less = bx < ax
+    is_king_y_less = by < ay
+
+    is_target_x_less = cx < ax
+    is_target_y_less = cy < ay
+
+    if is_king_x_less == is_target_x_less and is_king_y_less == is_target_y_less:
+        print("YES")
+    else:
+        print("NO")
+
+
+solve()
