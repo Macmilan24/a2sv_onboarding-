a5c8549632c6a9aed88f7931af26029cc815482d
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-07-07 23:12:55 +0300

    Created 'B - AB Flipping' by A2SV Hub

---
+++ A2SV Contest #21 07-Jul-2025/B - AB Flipping 374396.py
@@ -0,0 +1,30 @@
+# Problem: B - AB Flipping - https://codeforces.com/gym/619446/problem/B
+
+
+def solve():
+    n = int(input())
+    s = input()
+
+    first_a = -1
+    for i in range(n):
+        if s[i] == "A":
+            first_a = i
+            break
+
+    last_b = -1
+
+    for i in range(n - 1, -1, -1):
+        if s[i] == "B":
+            last_b = i
+            break
+
+    if first_a == -1 or last_b == -1 or first_a > last_b:
+        print(0)
+
+    else:
+        print(last_b - first_a)
+
+
+t = int(input())
+for _ in range(t):
+    solve()
