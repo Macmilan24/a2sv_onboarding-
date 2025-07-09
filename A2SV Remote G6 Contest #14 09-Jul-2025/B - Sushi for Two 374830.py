1efc904e2fbbe4f06042f15d81be4aab21ad90ca
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-07-09 18:24:01 +0300

    Created 'B - Sushi for Two' by A2SV Hub

---
+++ A2SV Remote G6 Contest #14 09-Jul-2025/B - Sushi for Two 374830.py
@@ -0,0 +1,21 @@
+# Problem: B - Sushi for Two - https://codeforces.com/gym/610550/problem/B
+
+n = int(input())
+a = list(map(int, input().split()))
+
+blocks = []
+count = 1
+
+for i in range(1, n):
+    if a[i] == a[i - 1]:
+        count += 1
+    else:
+        blocks.append(count)
+        count = 1
+blocks.append(count)
+
+res = 0
+for i in range(1, len(blocks)):
+    res = max(res, 2 * min(blocks[i], blocks[i - 1]))
+
+print(res)
