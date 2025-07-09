18e8ae7b9c561e54ac3b68901dbdcc1710119775
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-07-09 18:12:37 +0300

    Created 'A - Combination Lock' by A2SV Hub

---
+++ A2SV Remote G6 Contest #14 09-Jul-2025/A - Combination Lock 374828.py
@@ -0,0 +1,14 @@
+# Problem: A - Combination Lock - https://codeforces.com/gym/610550/problem/A
+
+n = int(input())
+original = input()
+target = input()
+
+moves = 0
+
+for i in range(n):
+    o = int(original[i])
+    t = int(target[i])
+    diff = abs(o - t)
+    moves += min(diff, 10 - diff)
+print(moves)
