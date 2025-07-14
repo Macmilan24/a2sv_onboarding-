665dd3d2025cd5f12949cf6e84daea284c15b01e
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-07-14 20:35:03 +0300

    Created 'E - The Case of the Missing Operations' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/E - The Case of the Missing Operations 376468.py
@@ -0,0 +1,42 @@
+# Problem: E - The Case of the Missing Operations - https://codeforces.com/gym/622136/problem/E
+
+import heapq
+
+
+def solve():
+    n = int(input())
+
+    heap = []
+    res = []
+
+    for _ in range(n):
+        line = input()
+
+        if line.startswith("insert"):
+            val = int(line.split()[1])
+            heapq.heappush(heap, val)
+            res.append(line)
+        elif line.startswith("removeMin"):
+            if not heap:
+                res.append("insert 0")
+            else:
+                heapq.heappop(heap)
+            res.append(line)
+        else:
+            val = int(line.split()[1])
+            while heap and heap[0] < val:
+                heapq.heappop(heap)
+                res.append("removeMin")
+
+            if not heap or heap[0] > val:
+                heapq.heappush(heap, val)
+                res.append(f"insert {val}")
+
+            res.append(line)
+
+    print(len(res))
+    for result in res:
+        print(result)
+
+
+solve()
