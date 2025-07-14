0c7e0ca19c29691ccf558681bf863eabfca61093
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-07-14 20:35:09 +0300

    Created 'D - Split the Difference!' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/D - Split the Difference! 376469.py
@@ -0,0 +1,25 @@
+# Problem: D - Split the Difference! - https://codeforces.com/gym/622136/problem/D
+
+def solve():
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    if k > n:
+        print(0)
+        return
+
+    total_cost = a[n - 1] - a[0]
+
+    diffs = []
+
+    for i in range(n - 1):
+        diffs.append(a[i + 1] - a[i])
+
+    diffs.sort(reverse=True)
+    for i in range(k - 1):
+        total_cost -= diffs[i]
+
+    print(total_cost)
+
+
+solve()
