4cc5435e60c2668d6d258dbfcec278dc27c964e3
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-07-29 21:53:19 +0300

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 29-Jul-2025/Climbing Stairs 380357.py
@@ -0,0 +1,14 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {1: 1, 2: 2}
+
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            
+            memo[n] = dp(n - 1) + dp(n - 2)
+            return memo[n]
+        
+        return dp(n)
