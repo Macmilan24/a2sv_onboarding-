412bc052794effa4dd7047371e7333a6187cda4c
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-08-14 20:48:32 +0300

    Created 'A - Frog 1' by A2SV Hub

---
+++ 14-Aug-2025/A - Frog 1 385084.py
@@ -0,0 +1,19 @@
+# Problem: A - Frog 1 - https://atcoder.jp/contests/dp/tasks/dp_a
+
+
+n = int(input())
+h = list(map(int, input().split()))
+
+if n == 2:
+    print(abs(h[1] - h[0]))
+else:
+    dp = [0] * n
+    dp[0] = 0
+    dp[1] = abs(h[1] - h[0])
+
+    for i in range(2, n):
+        cost1 = dp[i-1] + abs(h[i] - h[i-1])
+        cost2 = dp[i-2] + abs(h[i] - h[i-2])
+        dp[i] = min(cost1, cost2)
+
+    print(dp[n-1])
