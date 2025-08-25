be3a0112dca041208c085e0a910b519daad11afc
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-08-25 14:48:13 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 25-Aug-2025/Cirno's Perfect Bitmasks Classroom 388671.py
@@ -0,0 +1,16 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+def lowest_bit(x):
+    return x & -x
+
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    if x == 1:
+        print(3)
+        continue
+    lb = lowest_bit(x)
+    if x == lb:
+        print(x + 1)
+    else:
+        print(lb)
