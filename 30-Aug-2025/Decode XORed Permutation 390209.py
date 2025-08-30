110c49c9b54115bdb3ac53535e894193c07b4808
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-08-30 23:01:02 +0300

    Created 'Decode XORed Permutation' by A2SV Hub

---
+++ 30-Aug-2025/Decode XORed Permutation 390209.py
@@ -0,0 +1,22 @@
+# Problem: Decode XORed Permutation - https://leetcode.com/problems/decode-xored-permutation/description/?envType=problem-list-v2&envId=bit-manipulation
+
+class Solution:
+    def decode(self, encoded: List[int]) -> List[int]:
+        n = len(encoded) + 1
+        
+        total = 0
+        for i in range(1, n+1):
+            total ^= i
+        
+        odd_xor = 0
+        for i in range(1, len(encoded), 2):
+            odd_xor ^= encoded[i]
+        
+        first = total ^ odd_xor
+        
+
+        perm = [first]
+        for e in encoded:
+            perm.append(perm[-1] ^ e)
+        
+        return perm
