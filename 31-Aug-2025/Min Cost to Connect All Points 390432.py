22ca9e1634c8142da07c24d43aae6669b65785a1
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-08-31 20:36:14 +0300

    Created 'Min Cost to Connect All Points' by A2SV Hub

---
+++ 31-Aug-2025/Min Cost to Connect All Points 390432.py
@@ -0,0 +1,50 @@
+# Problem: Min Cost to Connect All Points - https://leetcode.com/problems/min-cost-to-connect-all-points/
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+    
+    def find(self, x):
+        while self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+    
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return False  
+        if self.rank[rx] < self.rank[ry]:
+            self.parent[rx] = ry
+        elif self.rank[rx] > self.rank[ry]:
+            self.parent[ry] = rx
+        else:
+            self.parent[ry] = rx
+            self.rank[rx] += 1
+        return True
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        edges = []
+        
+        for i in range(n):
+            for j in range(i+1, n):
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                edges.append((dist, i, j))
+        
+        edges.sort()
+        
+        uf = UnionFind(n)
+        total_cost = 0
+        edges_used = 0
+        
+        for dist, u, v in edges:
+            if uf.union(u, v):
+                total_cost += dist
+                edges_used += 1
+                if edges_used == n - 1: 
+                    break
+        
+        return total_cost
