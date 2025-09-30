b209e7270b8912ee02a51b116a36e294e80c47c8
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-09-30 20:50:28 +0300

    Created 'Top K Frequent Words' by A2SV Hub

---
+++ 30-Sep-2025/Top K Frequent Words 398094.py
@@ -0,0 +1,9 @@
+# Problem: Top K Frequent Words - https://leetcode.com/problems/top-k-frequent-words/
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = Counter(words)
+        
+        sorted_words = sorted(freq.keys(), key=lambda word: (-freq[word], word))
+        
+        return sorted_words[:k]
