cfe9fb4619ff0e468579e95fbb6ded2ff7cacea5
Samuel Dagne <120713570+Macmilan24@users.noreply.github.com>
2025-10-14 23:55:34 +0300

    Created 'Prime Palindrome' by A2SV Hub

---
+++ 14-Oct-2025/Prime Palindrome 403185.py
@@ -0,0 +1,32 @@
+# Problem: Prime Palindrome - https://leetcode.com/problems/prime-palindrome/description/?envType=problem-list-v2&envId=number-theory
+
+class Solution:
+    def primePalindrome(self, n: int) -> int:
+        def is_prime(x):
+            if x < 2:
+                return False
+            if x % 2 == 0:
+                return x == 2
+            i = 3
+            while i * i <= x:
+                if x % i == 0:
+                    return False
+                i += 2
+            return True
+        
+        if n <= 2:
+            return 2
+        if n <= 3:
+            return 3
+        if n <= 5:
+            return 5
+        if n <= 7:
+            return 7
+        if n <= 11:
+            return 11
+        
+        for half in range(1, 100000):
+            s = str(half)
+            pal = int(s + s[-2::-1])  
+            if pal >= n and is_prime(pal):
+                return pal
